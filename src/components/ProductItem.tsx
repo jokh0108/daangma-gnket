4e3bcc1ee487be41c12586760712804dcf59e613
jokh0108 <jokh0108@gmail.com>
2021-08-06 02:36:16 +0900

    [DG-012] fix: class 적용 버그 수정

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -13,29 +13,27 @@ function ProductItem({
   const now = new Date();
   const elapsed = now.getTime() - updatedAt.getTime();
   return (
-    <CustomLink to={`/products/${id}`}>
-      <div className="ProductItem">
-        <img className="ProductThumbnail" src={thumbnail} alt={title} />
-        <div className="ProductInfo">
-          <span className="ProductTitle">{title}</span>
-          <span className="ProductLocationAndUpdateAt">
-            {location} · {elapsed}초 전
-          </span>
-          <span className="ProductPrice">{price.toLocaleString()}원</span>
-          <div className="ProductInteractions">
-            {chats > 0 && (
-              <span className="ProductLike">
-                <IoHeartOutline />
-                {chats}
-              </span>
-            )}
-            {likes > 0 && (
-              <span className="ProductChat">
-                <IoChatbubblesOutline />
-                {likes}
-              </span>
-            )}
-          </div>
+    <CustomLink className="ProductItem" to={`/products/${id}`}>
+      <img className="ProductThumbnail" src={thumbnail} alt={title} />
+      <div className="ProductInfo">
+        <span className="ProductTitle">{title}</span>
+        <span className="ProductLocationAndUpdateAt">
+          {location} · {elapsed}초 전
+        </span>
+        <span className="ProductPrice">{price.toLocaleString()}원</span>
+        <div className="ProductInteractions">
+          {chats > 0 && (
+            <span className="ProductLike">
+              <IoHeartOutline />
+              {chats}
+            </span>
+          )}
+          {likes > 0 && (
+            <span className="ProductChat">
+              <IoChatbubblesOutline />
+              {likes}
+            </span>
+          )}
         </div>
       </div>
     </CustomLink>
